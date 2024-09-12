02203995f050507cb3d36076633362b8da00db71
RC_Student_lab <RC_Student_lab@10.0.0.98>
2024-09-12 09:47:09 +0000

    Third commit

--- src/main/java/com/mycompany/vowelcount1/VowelCount1.java
+++ src/main/java/com/mycompany/vowelcount1/VowelCount1.java
@@ -3,14 +3,36 @@
  */
 
 package com.mycompany.vowelcount1;
+import java.util.Scanner;
 
 /**
  *
  * @author RC_Student_lab
  */
 public class VowelCount1 {
+    
+    public static char countVowels(String word){
+        word = word.toLowerCase();
+        
+        String vowels = "aeiou";
+        
+        for(int i = 0; 0 < word.length(); i++){
+            char currentChar = word.charAt(i);
+            
+            if (vowels.indexOf(currentChar) != -1){
+                return currentChar;
+            }
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter a word: ");
+        String word = input.next();
+        
+        System.out.println("Vowel Found: " + VowelCount1.countVowels(word));
     }
 }
